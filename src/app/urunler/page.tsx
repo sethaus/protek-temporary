360960c02b60ba87f3b496c9fba19168865fd454
sethaus <denizhan@setupsuit.com>
2025-07-01 22:39:10 +0300

    feat: add client-side rendering directive to products page

--- src/app/urunler/page.tsx
+++ src/app/urunler/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { BeakerIcon, CubeIcon, WrenchIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
